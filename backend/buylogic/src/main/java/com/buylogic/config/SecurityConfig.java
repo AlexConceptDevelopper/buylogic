0a14462d8f870ffb7d156ad16761612df8b85702
AlexConceptDevelopper <alexandre.cubizolle@hotmail.fr>
2026-09-02 21:30:23 +0200

    add Enable WebSecurity

--- backend/buylogic/src/main/java/com/buylogic/config/SecurityConfig.java
+++ backend/buylogic/src/main/java/com/buylogic/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.buylogic.security.JwtAuthFilter;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
