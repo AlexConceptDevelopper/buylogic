aee44f8be4d2654fb70db1a2b6ae52186aa6c4f4
AlexConceptDevelopper <alexandre.cubizolle@hotmail.fr>
2026-09-04 19:48:49 +0200

    fix register

--- backend/buylogic/src/main/java/com/buylogic/controller/AuthController.java
+++ backend/buylogic/src/main/java/com/buylogic/controller/AuthController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,9 +26,7 @@ public class AuthController {
 
         RegisterResponse response = authService.register(request);
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(response);
+        return ResponseEntity.ok(response); // Passe en 200 OK
     }
 
     @PostMapping("/login")
